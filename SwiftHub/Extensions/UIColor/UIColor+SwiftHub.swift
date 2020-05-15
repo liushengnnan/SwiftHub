b9dfc734583b681426ea96ee6d30ad4c1447444d
shengnan.liu <sunny.liusn@gmail.com>
2020-05-15 21:42:18 +0800

    delete

--- SwiftHub/Extensions/UIColor/UIColor+SwiftHub.swift
+++ SwiftHub/Extensions/UIColor/UIColor+SwiftHub.swift
@@ -36,14 +36,3 @@ extension UIColor {
         return themeService.type.associatedObject.text
     }
 }
-
-extension UIColor {
-
-    var brightnessAdjustedColor: UIColor {
-        var components = self.cgColor.components
-        let alpha = components?.last
-        components?.removeLast()
-        let color = CGFloat(1-(components?.max())! >= 0.5 ? 1.0 : 0.0)
-        return UIColor(red: color, green: color, blue: color, alpha: alpha!)
-    }
-}
